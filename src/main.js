b8a034c8588e60a5f8bb3a6cc45122c0039cd211
dawn <805917076@qq.com>
2017-05-20 22:55:19 +0800

    cs

---
+++ src/main.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: "",
+    paths: {
+        "jquery": "https://cdn.bootcss.com/jquery/3.1.1/jquery.min",
+        "vue": "https://cdn.bootcss.com/vue/2.2.1/vue.min",
+        "moment": "https://cdn.bootcss.com/moment.js/2.17.0/moment.min",
+        "dcore" : "./js/dcore",
+    }
+});
+
+ 
